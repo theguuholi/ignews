771de47b5463c1e10d997143c8fb6074feeb5426
Gustavo Oliveira <g.92oliveira@gmail.com>
2023-03-16 09:11:07 -0300

    next auth user

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { fauna } from "@/src/services/fauna";
+import { query as q } from "faunadb";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { signIn } from "next-auth/react";
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
@@ -9,5 +12,30 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
-}
-export default NextAuth(authOptions)
+  // jwt: {
+  //   signingKey: process.env.SIGNIN_KEY
+  // },
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("users_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create("users", { data: { email } }),
+            q.Get(q.Match(q.Index("users_by_email"), q.Casefold(user.email)))
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
+};
+export default NextAuth(authOptions);
