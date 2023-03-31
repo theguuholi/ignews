0c39bfab6a09e2082117d63ee24416d0a7ce3a2a
Gustavo Oliveira <g.92oliveira@gmail.com>
2023-03-31 11:18:32 -0300

    fix handle subscribe

--- src/componens/SubscribeButton/index.tsx
+++ src/componens/SubscribeButton/index.tsx
@@ -2,32 +2,41 @@ import { api } from "../../services/api";
 import { getStripeJs } from "@/src/services/stripe-js";
 import { signIn, useSession } from "next-auth/react";
 import styles from "./styles.module.scss";
+import { useRouter } from "next/router";
 
 // lugares seguros getServerSideProps (SSR), getStatisProps(SSSG) API ROUTEs
 export function SubscribeButton() {
-    const session = useSession()
+  const session = useSession();
+  const router = useRouter();
 
-    async function handleSubscribe(){
-
-        if(!session){
-            signIn("github")
-            return;
-        }
-
+  async function handleSubscribe() {
+    if (session.status === "unauthenticated") {
+      signIn("github");
+      return;
+    } else {
+      if (session.data.activeSubscription) {
+        router.push("/posts");
+        return;
+      } else {
         try {
-            const response = await api.post("/subscribe")
-            const {sessionId} = response.data
-            const stripe =  await getStripeJs()
-            await stripe.redirectToCheckout({sessionId})
+          const response = await api.post("/subscribe");
+          const { sessionId } = response.data;
+          const stripe = await getStripeJs();
+          await stripe.redirectToCheckout({ sessionId });
         } catch (error) {
-            alert(error.message)
+          alert(error.message);
         }
-
+      }
     }
+  }
 
-    return (
-        <button type="button" className={styles.subscribeButton} onClick={handleSubscribe}>
-            Subscribe now!
-        </button>
-    );
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now!
+    </button>
+  );
 }
