a1d64907eb4569ad6b658f818fe3c5815d852251
Gustavo Oliveira <g.92oliveira@gmail.com>
2023-03-31 17:31:59 -0300

    change to use rust

--- src/componens/ActiveLink/ActiveLink.spec.tsx
+++ src/componens/ActiveLink/ActiveLink.spec.tsx
@@ -1,4 +1,6 @@
+
 import { render } from "@testing-library/react";
+
 import { ActiveLink } from ".";
 
 jest.mock("next/router", () => {
@@ -15,9 +17,7 @@ describe("Active Link component", () => {
   test("active link renders correctly", () => {
     //   const { _debug, getByText } =
     const { getByText } = render(
-      <ActiveLink href="/" activeClassName="active">
-        <a>Home</a>
-      </ActiveLink>
+      <ActiveLink href="/" activeClassName="active">Home</ActiveLink>
     );
 
     //   debug();
@@ -26,9 +26,7 @@ describe("Active Link component", () => {
 
   it("adds active class if the link as currently", () => {
     const { getByText } = render(
-      <ActiveLink href="/" activeClassName="active">
-        <a>Home</a>
-      </ActiveLink>
+      <ActiveLink href="/" activeClassName="active">Home</ActiveLink>
     );
 
     expect(getByText("Home")).toHaveClass("active");
